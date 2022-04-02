3a21dc29e043bb0c22733a1fd106e8b851abd23a
Jakub Povinec <jakub.povinec@gmail.com>
2022-04-02 21:48:48 +0200

    changed data generator

--- src/generator.py
+++ src/generator.py
@@ -1,3 +1,4 @@
+import cv2
 import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
 import nibabel as nib
@@ -8,7 +9,7 @@ import random
 
 # https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
 class BratsGen(tf.keras.utils.Sequence):
-    def __init__(self, flair_list, t1ce_list, t2_list, mask_list, img_dim=(128, 128, 128),
+    def __init__(self, flair_list, t1ce_list, t2_list, mask_list, img_dim=(128,128,128),
                  img_channels=3, classes=4, batch_size=2, segmenting_subregion=0):
         self.batch_size = batch_size
         self.classes = classes
@@ -21,18 +22,26 @@ class BratsGen(tf.keras.utils.Sequence):
         self.segmenting_subregion = segmenting_subregion
 
     def __len__(self):
-        return len(self.target_img_paths) // self.batch_size
+        return len(self.flair_list) // self.batch_size
 
     def __getitem__(self, idx):
         i = idx * self.batch_size
-        batch_flair = self.flair_list[i: i + self.batch_size]
-        batch_t1ce = self.t1ce_list[i: i + self.batch_size]
-        batch_t2 = self.t2_list[i: i + self.batch_size]
-        batch_mask = self.mask_list[i: i + self.batch_size]
+        batch_flair = self.flair_list[i : i + self.batch_size]
+        batch_t1ce = self.t1ce_list[i : i + self.batch_size]
+        batch_t2 = self.t2_list[i : i + self.batch_size]
+        batch_mask = self.mask_list[i : i + self.batch_size]
 
         X, y = self.__data_generation(batch_flair, batch_t1ce, batch_t2, batch_mask)
 
         return X, y
+
+    # https://stackoverflow.com/a/44007180
+    def __get_dimensions(self, img, scale=.7):
+        center_x, center_y = img.shape[1] / 2, img.shape[0] / 2
+        width_scaled, height_scaled = img.shape[1] * scale, img.shape[0] * scale
+        left_x, right_x = center_x - width_scaled / 2, center_x + width_scaled / 2
+        top_y, bottom_y = center_y - height_scaled / 2, center_y + height_scaled / 2
+        return int(left_x), int(right_x), int(top_y), int(bottom_y)
 
     # normalisation based on https://stackoverflow.com/a/59601298
     def __normalise(self, image):
@@ -41,19 +50,45 @@ class BratsGen(tf.keras.utils.Sequence):
         return image
 
     def __data_generation(self, flair_list, t1ce_list, t2_list, mask_list):
-        # X = np.zeros((self.batch_size*self.img_dim[2], self.img_dim[0], self.img_dim[1], self.img_channels))
-        # y = np.zeros((self.batch_size*self.img_dim[2], self.img_dim[0], self.img_dim[1]))
-        # Y = np.zeros((self.batch_size*self.img_dim[2], self.img_dim[0], self.img_dim[1], 4))
+        #X = np.zeros((self.batch_size*self.img_dim[2], self.img_dim[0], self.img_dim[1], self.img_channels))
+        #y = np.zeros((self.batch_size*self.img_dim[2], self.img_dim[0], self.img_dim[1]))
+        #Y = np.zeros((self.batch_size*self.img_dim[2], self.img_dim[0], self.img_dim[1], 4))
 
         images = []
         masks = []
         for flair_name, t1ce_name, t2_name, mask_name in zip(flair_list, t1ce_list, t2_list, mask_list):
-            # ==================== Sequences ====================#
             flair = nib.load(flair_name).get_fdata()
             t1ce = nib.load(t1ce_name).get_fdata()
             t2 = nib.load(t2_name).get_fdata()
             mask = nib.load(mask_name).get_fdata()
 
+            # crop the images and mask
+            left_x, right_x, top_y, bottom_y = self.__get_dimensions(flair[:,:,0])
+            #print(left_x, right_x, top_y, bottom_y)
+            flair = flair[left_x:right_x, top_y:bottom_y,:]
+            t1ce = t1ce[left_x:right_x, top_y:bottom_y,:]
+            t2 = t2[left_x:right_x, top_y:bottom_y,:]
+            mask = mask[left_x:right_x, top_y:bottom_y,:]
+
+            volume_start = 13
+            volume_end = 141
+            tmp_flair = np.zeros((128, 128, 128))
+            tmp_t1ce = np.zeros((128, 128, 128))
+            tmp_t2 = np.zeros((128, 128, 128))
+            tmp_mask = np.zeros((128, 128, 128))
+
+            # resize the images and mask
+            for i in range(128):
+                tmp_flair[:,:,i] = cv2.resize(flair[:,:,i+volume_start], (128, 128))
+                tmp_t1ce[:,:,i] = cv2.resize(t1ce[:,:,i+volume_start], (128, 128))
+                tmp_t2[:,:,i]= cv2.resize(t2[:,:,i+volume_start], (128, 128))
+                tmp_mask[:,:,i]= cv2.resize(mask[:,:,i+volume_start], (128, 128))
+            flair = tmp_flair
+            t1ce = tmp_t1ce
+            t2 = tmp_t2
+            mask = tmp_mask
+
+            #==================== Sequences ====================#
             # normalise
             flair = self.__normalise(flair)
             t1ce = self.__normalise(t1ce)
@@ -66,9 +101,9 @@ class BratsGen(tf.keras.utils.Sequence):
                 image = np.stack([flair, t1ce], axis=3)
 
             # crop the image to 128x128x128
-            image = image[56:184, 56:184, 13:141]
+            #image = image[56:184, 56:184, 13:141]
 
-            # ==================== Mask ====================#
+            #==================== Mask ====================#
             # change label 4 to label 3 because label 3 is empty
             mask[mask == 4] = 3
 
@@ -88,24 +123,24 @@ class BratsGen(tf.keras.utils.Sequence):
                 mask[mask == 3] = 1
 
             # crop the mask to 128x128x128
-            mask = mask[56:184, 56:184, 13:141]
+            #mask = mask[56:184, 56:184, 13:141]
 
             # encode
             if self.segmenting_subregion == 0:
                 mask = to_categorical(mask, num_classes=4)
             else:
-                # mask = to_categorical(mask, num_classes=2)
+                #mask = to_categorical(mask, num_classes=2)
                 mask = tf.one_hot(mask, 1, on_value=0, off_value=1)
 
             images.append(image)
             masks.append(mask)
 
-            # X[j+(VOLUME_SLICES*c),:,:,0] = cv2.resize(flair[:,:,j+VOLUME_START_AT], (IMG_SIZE, IMG_SIZE))
-            # X[j+(VOLUME_SLICES*c),:,:,1] = cv2.resize(ce[:,:,j+VOLUME_START_AT], (IMG_SIZE, IMG_SIZE))
-            # y[j +VOLUME_SLICES*c,:,:] = cv2.resize(seg[:,:,j+VOLUME_START_AT], (IMG_SIZE, IMG_SIZE))
+            #X[j+(VOLUME_SLICES*c),:,:,0] = cv2.resize(flair[:,:,j+VOLUME_START_AT], (IMG_SIZE, IMG_SIZE))
+            #X[j+(VOLUME_SLICES*c),:,:,1] = cv2.resize(ce[:,:,j+VOLUME_START_AT], (IMG_SIZE, IMG_SIZE))
+            #y[j +VOLUME_SLICES*c,:,:] = cv2.resize(seg[:,:,j+VOLUME_START_AT], (IMG_SIZE, IMG_SIZE))
 
         images = np.array(images)
         masks = np.array(masks)
 
-        # return images/np.max(images), masks
+        #return images/np.max(images), masks
         return images, masks
